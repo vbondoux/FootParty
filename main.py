98b3f6bc14782a4a6d5c473ca2edd8f5ea73b6b6
vbondoux <vbondoux@gmail.com>
2025-04-14 17:14:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from openai_client import create_thread, send_message, wait_for_run_completion, get_response
 
 app = FastAPI()
